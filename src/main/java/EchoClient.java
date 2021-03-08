f12e6ebbdd534871cf26425b30565f443e768a6f
AlexKenfelt <cph-ak428@cphbusiness.dk>
2021-03-08 15:17:34 +0100

    EchoServer connection

--- src/main/java/EchoClient.java
+++ src/main/java/EchoClient.java
@@ -3,15 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class EchoClient {
+    Socket socket;
+    PrintWriter pw;
+    Scanner scanner;
+
+    //String serverIP = "123. 123.0.4";
+    // int serverPort = 8088;
+
+
     public static void main(String[] args) {
-
         EchoClient echoClient = new EchoClient();
-
         echoClient.runProgram();
+    }
+        /*try {
+            new EchoClient().connect("localhost",8088);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        echoClient.runProgram();/*
 
     }
+
+    /*public void connect (String address, int port) throws IOException {
+        socket = new Socket(address,port);
+        pw = new PrintWriter(socket.getOutputStream(),true);
+        scanner = new Scanner(socket.getInputStream());
+        System.out.println(scanner.nextLine());
+    }*/
 
     private void runProgram() {
         try {
---
+++ src/main/java/EchoServer.java
@@ -0,0 +1,67 @@
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.Buffer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.*;
+
+public class EchoServer {
+    //public static final int DEFAULT_PORT = 2345;
+    ConcurrentMap<String,Socket> hashmap = new ConcurrentHashMap<>();
+    BlockingQueue<String> allmsg = new ArrayBlockingQueue<String>(200);
+
+
+
+    public static void main(String[] args) {
+        int port = 8088;
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        EchoServer echoServer = new EchoServer();
+        echoServer.runProgram();
+    }
+
+    private void runProgram() {
+
+
+        try {
+            ServerSocket ss = new ServerSocket(8088);
+            while (true) {
+                Socket client = ss.accept();
+
+                InputStreamReader ir = new InputStreamReader(client.getInputStream());
+                PrintWriter printWriter = new PrintWriter(client.getOutputStream(),true);
+                BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                //TODO; Handle input like this; CONNECT#Kurt
+                String input = br.readLine();
+                System.out.println(input);
+                String name = input.substring(8);
+                        //CONNECT#Kurt
+                        //SEND#Peter#Hello Peter
+                printWriter.println("Farvel");
+                client.close();
+                // TODO; klargøre den delte resource til beskederne;
+                //TODO; læg klienten ind i en delt resource, så man kan slå den op på navnet (hashmap)
+
+                // skal laves imorgen!!!
+                //ClientHandler cl = new ClientHandler(name,client,allmsg);
+
+                //cl.greeting();
+                //cl.protocol();
+            }
+
+            //hashmap.put("Kurt",client);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
